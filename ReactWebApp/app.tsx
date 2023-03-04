4aa757aa107cd192d5977d78c7bb7e89d1e48612
Tamassp <petertamas05@gmail.com>
2023-03-04 14:10:18 +0100

    React App Initialized

---
+++ ReactWebApp/app.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function App() {
+return (
+<div>{'react setup from scratch without cra testing ts'}</div>
+);
+}
+export default App;
