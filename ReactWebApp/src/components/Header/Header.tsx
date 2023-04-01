52c03426142a00ea248327debc8d83ec52b2d4cf
Tamassp <petertamas05@gmail.com>
2023-04-01 10:03:06 +0200

    Menu Screen Layout

--- ReactWebApp/src/components/Header/Header.tsx
+++ ReactWebApp/src/components/Header/Header.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"
 
-export interface TitleProps {
+export interface HeaderProps {
     text: string,
     style?: React.CSSProperties
 }
 
-const Header = ({text, style}: TitleProps) => {
+const Header = ({text, style}: HeaderProps) => {
     return (
         <h2 style={style}>{text}</h2>
     )
--- ReactWebApp/src/components/SubHeader/SubHeader.tsx
+++ ReactWebApp/src/components/SubHeader/SubHeader.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"
 
-export interface TitleProps {
+export interface SubHeaderProps {
     text: string,
     style?: React.CSSProperties
 }
 
-const SubHeader = ({text, style}: TitleProps) => {
+const SubHeader = ({text, style}: SubHeaderProps) => {
     return (
         <h3 style={style}>{text}</h3>
     )
