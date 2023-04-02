1cf180dad45da52ea90eb1af48619d8c9428b9a3
Michal Pupák <michal.pupak@gmail.com>
2023-04-02 13:04:41 +0200

    Remove azure-pipelines.yml

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -6,7 +6,7 @@ using Service.Email.Helper;
 using Service.JWT.Model;
 using WebApi.Registry;
 
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -87,7 +87,7 @@ builder.Services.AddApiVersioning(opt =>
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: MyAllowSpecificOrigins,
+    options.AddPolicy(name: myAllowSpecificOrigins,
                       policy =>
                       {
                           //   policy.WithOrigins("http://localhost:8080/",
@@ -115,7 +115,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors(MyAllowSpecificOrigins);
+app.UseCors(myAllowSpecificOrigins);
 
 app.UseAuthorization();
 
