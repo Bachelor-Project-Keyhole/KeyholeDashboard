770f1a1edbe1bcd346c45c97211b14eb6a2c7553
Michal Pupák <michal.pupak@gmail.com>
2023-05-14 12:18:12 +0200

    Implement global error handling middleware

---
+++ Infrastructure/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,56 @@
+using System.Net;
+using System.Text.Json;
+using Domain.Exceptions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Middlewares;
+
+public class ErrorHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception exception)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+
+            switch (exception)
+            {
+                case OrganizationNotFoundException:
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+                case DataPointKeyNotFoundException:
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+                default:
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+            
+            var result = JsonSerializer.Serialize(new { errorMessage = exception.Message });
+            await response.WriteAsync(result);
+            Console.WriteLine($"EXCEPTION: {exception.Message}");
+        }
+    }
+}
+
+public static class ErrorHandlerMiddlewareExtensions
+{
+    public static IApplicationBuilder UseErrorHandlerMiddleware(
+        this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ErrorHandlerMiddleware>();
+    } 
+}
